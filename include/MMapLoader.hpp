62618ada14e2bc68f2986a9982f64ec03dc0603e
Thomas <th.fchs@gmail.com>
2025-12-10 00:48:07 +0100

    refactoring du header

--- include/MMapLoader.hpp
+++ include/MMapLoader.hpp
@@ -1,29 +1,23 @@
 #pragma once // permet au fichier d'être inclus au plus une fois
 
+#include <cstddef>     // size_t
 #include <string_view> // std::string_view
-#include <cstddef> // size_t
 
 class MMapLoader {
-public:
-    
+  public:
     // constructeur
     explicit MMapLoader(std::string_view file);
 
     // destructeur
     ~MMapLoader();
 
-    
-    MMapLoader (const MMapLoader&) = delete; // interdiction de la copie
-    MMapLoader& operator = (const MMapLoader&) = delete; // interdiction de l'assignation
-
+    MMapLoader(const MMapLoader &) = delete;            // interdiction de la copie
+    MMapLoader &operator=(const MMapLoader &) = delete; // interdiction de l'assignation
 
     std::string_view getView() const; // retourne une vue en lecture seule
 
-private:
-
-    void* m_addr; // adresse du pointer de mmap
+  private:
+    void *m_addr;    // adresse du pointer de mmap
     size_t m_length; // taille du mapping dans la mémoire
-    int m_fd;  
-
-
+    int m_fd;
 };
